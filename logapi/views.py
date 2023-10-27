5fa0975b63bf07b7523fbd407038e834cbac2d71
Andrew <andrew.lai@bell.ca>
2023-10-26 20:52:40 -0700

    - Fix indentation in views.py

--- logapi/views.py
+++ logapi/views.py
@@ -30,54 +30,54 @@ def get_log(request):
   logging.info("Reading file %s, keyword: %s, lines: %s", file_path, keyword, last_n_lines)
 
   try:
-      with open(file_path, 'rb') as file:
-        line_count = 0
-        curr_line = ''
-        lines_buffer = []
-        
-        file.seek(0, os.SEEK_END)
+    with open(file_path, 'rb') as file:
+      line_count = 0
+      curr_line = ''
+      lines_buffer = []
+      
+      file.seek(0, os.SEEK_END)
+      curr_pos = file.tell()
+
+      # the file is empty, just return empty json response here by setting last_n_lines to -1
+      if curr_pos == 0:
+        last_n_lines = -1
+      else:  
+        final_char = curr_pos == 1
+        # Move file cursor to 1 char before end of file, so we can read last char
+        file.seek(-1, os.SEEK_CUR)
+
+      while line_count < last_n_lines:
+        curr_char = file.read(1)
+        # TODO Support encodings with more than 1 byte per char, ie UTF-16 or japanese
+        curr_char = curr_char.decode('utf-8')
         curr_pos = file.tell()
 
-        # the file is empty, just return empty json response here by setting last_n_lines to -1
-        if curr_pos == 0:
-          last_n_lines = -1
-        else:  
-          final_char = curr_pos == 1
-          # Move file cursor to 1 char before end of file, so we can read last char
-          file.seek(-1, os.SEEK_CUR)
-
-        while line_count < last_n_lines:
-          curr_char = file.read(1)
-          # TODO Support encodings with more than 1 byte per char, ie UTF-16 or japanese
-          curr_char = curr_char.decode('utf-8')
-          curr_pos = file.tell()
-
-          if curr_pos == 1:
-            # we've hit the final character, need to append the line
-            final_char = True
-            if curr_char != '\n':
-              curr_line = curr_char + curr_line
-
-          # if hit newline, determine whether it passes keyword filter before appending to return
-          if curr_char == '\n' or final_char:
-            if curr_line and (not keyword or keyword.lower() in curr_line.lower()):
-              lines_buffer.append(curr_line)
-              line_count = line_count + 1
-            curr_line = ''
-            if final_char:
-              break
-          else:
+        if curr_pos == 1:
+          # we've hit the final character, need to append the line
+          final_char = True
+          if curr_char != '\n':
             curr_line = curr_char + curr_line
 
-          # move back max of 2 char since on above file.read(1) we iterate 1 forwards
-          # so every iteration of while loop our cursor -2+1
-          file.seek(-2, os.SEEK_CUR)    
+        # if hit newline, determine whether it passes keyword filter before appending to return
+        if curr_char == '\n' or final_char:
+          if curr_line and (not keyword or keyword.lower() in curr_line.lower()):
+            lines_buffer.append(curr_line)
+            line_count = line_count + 1
+          curr_line = ''
+          if final_char:
+            break
+        else:
+          curr_line = curr_char + curr_line
 
-        response_data = {
-          'lines': line_count,
-          'logs': lines_buffer
-        }
-        return JsonResponse(response_data)
+        # move back max of 2 char since on above file.read(1) we iterate 1 forwards
+        # so every iteration of while loop our cursor -2+1
+        file.seek(-2, os.SEEK_CUR)    
+
+      response_data = {
+        'lines': line_count,
+        'logs': lines_buffer
+      }
+      return JsonResponse(response_data)
   except FileNotFoundError:
       return JsonResponse({'error': 'File not found'}, status=404)
   except Exception as e:
