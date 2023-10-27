ab94db287f9c2bd4de6ff0f90bb822b69abf08a3
Andrew <andrew.lai@bell.ca>
2023-10-26 18:25:25 -0700

    Check in test files

--- tests/logapi_test.py
+++ tests/logapi_test.py
@@ -7,7 +7,7 @@ from django.test import TestCase
 class LogApiTestCase(TestCase):
 
   def test_log_api_endpoint(self):
-    response = self.client.get('/api/log/', {'filename': 'test.log'})
+    response = self.client.get('/api/log/', {'filename': 'test.txt'})
     self.assertEqual(response.status_code, 200)
     response_dict = json.loads(response.content)
     # file has 10 lines total
@@ -17,7 +17,7 @@ class LogApiTestCase(TestCase):
     self.assertEqual(response_dict['logs'][9], 'Test line 1')
 
   def test_log_api_n_lines_param(self):
-    response = self.client.get('/api/log/', {'filename': 'test.log', 'n': 5})
+    response = self.client.get('/api/log/', {'filename': 'test.txt', 'n': 5})
     self.assertEqual(response.status_code, 200)
     response_dict = json.loads(response.content)
     self.assertEqual(response_dict['lines'], 5)
@@ -26,21 +26,21 @@ class LogApiTestCase(TestCase):
     self.assertEqual(response_dict['logs'][4], 'Test line 6')
 
   def test_log_api_single_char(self):
-    response = self.client.get('/api/log/', {'filename': 'single_char.log'})
+    response = self.client.get('/api/log/', {'filename': 'single_char.txt'})
     self.assertEqual(response.status_code, 200)
     response_dict = json.loads(response.content)
     self.assertEqual(response_dict['lines'], 1)
     self.assertEqual(response_dict['logs'][0], '')
 
   def test_log_api_empty_file(self):
-    response = self.client.get('/api/log/', {'filename': 'empty.log'})
+    response = self.client.get('/api/log/', {'filename': 'empty.txt'})
     self.assertEqual(response.status_code, 200)
     response_dict = json.loads(response.content)
     self.assertEqual(response_dict['lines'], 0)
     self.assertEqual(len(response_dict['logs']), 0)
 
   def test_log_api_single_line(self):
-    response = self.client.get('/api/log/', {'filename': 'single_line.log'})
+    response = self.client.get('/api/log/', {'filename': 'single_line.txt'})
     self.assertEqual(response.status_code, 200)
     response_dict = json.loads(response.content)
     self.assertEqual(response_dict['lines'], 1)
