e510d434fe96f06126a28a61f95f15d4d1d2f457
Andrew <andrew.lai@bell.ca>
2023-10-26 21:31:58 -0700

    Self code review

--- logapi/views.py
+++ logapi/views.py
@@ -21,11 +21,11 @@ def get_log(request):
     return JsonResponse({'error': 'Param filename must have one of the extensions %s' % str(ALLOWED_EXTENSIONS)}, status=422)
 
   keyword = request.GET.get('keyword', '')
-
   str_n_lines = request.GET.get('n', '100')
   if not str_n_lines.isdigit():
     return JsonResponse({'error': 'Param n must be digit'}, status=422)
   last_n_lines = int(str_n_lines)
+
   file_path = os.path.join(LOGS_DIRECTORY, filename)
   logging.info("Reading file %s, keyword: %s, lines: %s", file_path, keyword, last_n_lines)
 
@@ -34,43 +34,43 @@ def get_log(request):
       line_count = 0
       curr_line = ''
       lines_buffer = []
-      
+
       file.seek(0, os.SEEK_END)
       curr_pos = file.tell()
+      final_char = False
 
       # the file is empty, just return empty json response here by setting last_n_lines to -1
       if curr_pos == 0:
         last_n_lines = -1
       else:  
-        final_char = curr_pos == 1
         # Move file cursor to 1 char before end of file, so we can read last char
         file.seek(-1, os.SEEK_CUR)
 
       while line_count < last_n_lines:
-        curr_char = file.read(1)
-        # TODO Support encodings with more than 1 byte per char, ie UTF-16 or japanese
-        curr_char = curr_char.decode('utf-8')
+        curr_byte = file.read(1)
+        # TODO Support encodings with more than 1 byte per char, ie non ascii, japanese
+        curr_char = curr_byte.decode('utf-8')
         curr_pos = file.tell()
+
+        if curr_char != '\n':
+          curr_line = curr_char + curr_line
 
         if curr_pos == 1:
           # we've hit the final character, need to append the line
           final_char = True
-          if curr_char != '\n':
-            curr_line = curr_char + curr_line
 
-        # if hit newline, determine whether it passes keyword filter before appending to return
-        if curr_char == '\n' or final_char:
+        append_line = curr_char == '\n' or final_char 
+        if append_line:
           if curr_line and (not keyword or keyword.lower() in curr_line.lower()):
             lines_buffer.append(curr_line)
             line_count = line_count + 1
           curr_line = ''
-          if final_char:
-            break
-        else:
-          curr_line = curr_char + curr_line
 
-        # move back max of 2 char since on above file.read(1) we iterate 1 forwards
-        # so every iteration of while loop our cursor -2+1
+        if final_char:
+          break  
+
+        # move back max of 2 bytes since on above file.read(1) we iterate 1 forwards
+        # so every iteration of while loop our cursor -2+1=-1 byte backwards
         file.seek(-2, os.SEEK_CUR)    
 
       response_data = {
